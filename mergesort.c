15494608d0c8d70cb1c969c321cd4071b1289dba
Annmaria Thankachan <165566292+AnnmariaThankachan76@users.noreply.github.com>
2024-11-13 13:54:49 +0530

    mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -20,6 +20,7 @@ void mergesort(int a[], int low, int high){
  if(low<high){
   int mid=(low+high)/2;
   mergesort(a,low,mid);
+	 mergesort(a,mid+1,high);
   merge(a,low,mid,high);
   }}
 void merge(int a[], int low, int mid, int high){
